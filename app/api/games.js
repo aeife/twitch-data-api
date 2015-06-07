8c15c91fd2378c5df0c54abc4d1f78dca6a9aedf
aeife <andre.eife@gmail.com>
2015-06-07 22:30:04 +0200

    support alphabetical ordering

--- app/api/games.js
+++ app/api/games.js
@@ -60,9 +60,13 @@ router.route('/games')
         } else if (options.ratio) {
           return ((d1.channels > 0) ? d1.viewers / d1.channels : 0) - ((d2.channels > 0) ? d2.viewers / d2.channels : 0);
         } else if (options.order === 'desc') {
-          return d2[options.sortAttr] - d1[options.sortAttr];
+          if(d1[options.sortAttr] < d2[options.sortAttr]) return 1;
+          if(d1[options.sortAttr] > d2[options.sortAttr]) return -1;
+          return 0;
         } else {
-          return d1[options.sortAttr] - d2[options.sortAttr];
+          if(d1[options.sortAttr] < d2[options.sortAttr]) return -1;
+          if(d1[options.sortAttr] > d2[options.sortAttr]) return 1;
+          return 0;
         }
       });
 
