55da74f18c0be2990e683ace6f65dc7a299c7ac9
aeife <andre.eife@gmail.com>
2015-06-07 03:13:12 +0200

    sort query

--- app/api/games.js
+++ app/api/games.js
@@ -1,31 +1,88 @@
 var express = require('express');
 var Game = require('../models/game').model;
 var router = express.Router();
+
+var queryGames = function (attr, sortType, fullData, order) {
+    var group = {
+        _id: "$_id",
+        name: { $first: "$name" },
+        viewers: {},
+        channels: {}
+    };
+    group.viewers['$' + sortType] = "$stats.viewers";
+    group.channels['$' + sortType] = "$stats.channels";
+
+    if (fullData) {
+      group.stats = {
+         $push: {
+             viewers: "$stats.viewers",
+             channels: "$stats.channels"
+         }
+      };
+    }
+
+    var sort = {};
+    if (order == 'asc') {
+      sort[attr] = 1;
+    } else {
+      sort[attr] = -1;
+    }
+
+    return Game
+      .aggregate()
+      .unwind("stats")
+      .group(group)
+      .sort(sort);
+};
+
 router.route('/games')
     // get game list
     .get(function(req, res) {
-        Game
-            .find()
-            .limit(req.query.limit)
-            .skip(req.query.offset)
-            .exec(function(err, games) {
-                if (err) {
-                    res.send(err);
-                }
+        queryGames(req.query.sortAttr, req.query.sortType, req.query.fullData, req.query.order)
+          .limit(parseInt(req.query.limit))
+          .skip(parseInt(req.query.offset || 0))
+          .exec(function(err, games) {
+              if (err) {
+                  res.send(err);
+              }
 
-                Game.count(function (err, count) {
-                    if (err) {
-                        res.send(err);
-                    }
+              Game.count(function (err, count) {
+                  if (err) {
+                      res.send(err);
+                  }
 
-                    res.json({
-                        games: games,
-                        limit: req.query.limit,
-                        offset: req.query.offset,
-                        count: count
-                    });
-                });
-            });
+                  res.json({
+                      games: games,
+                      limit: req.query.limit,
+                      offset: req.query.offset,
+                      count: count
+                  });
+              });
+          });
+
+
+        // Game
+        //     .find()
+        //     .limit(req.query.limit)
+        //     .skip(req.query.offset)
+        //     .exec(function(err, games) {
+        //         if (err) {
+        //             res.send(err);
+        //         }
+        //
+        //         Game.count(function (err, count) {
+        //             if (err) {
+        //                 res.send(err);
+        //             }
+        //
+        //             res.json({
+        //                 games: games,
+        //                 limit: req.query.limit,
+        //                 offset: req.query.offset,
+        //                 count: count
+        //             });
+        //         });
+        //     });
     });
 
 module.exports = router;
