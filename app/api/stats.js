d11fe37e4a36fbc1dd57817d00f286eb640af6b3
aeife <andre.eife@gmail.com>
2015-07-24 00:15:11 +0200

    condensed total stats payload

--- app/api/stats.js
+++ app/api/stats.js
@@ -6,13 +6,56 @@ var _ = require('lodash');
 
 router.route('/stats')
   .get(function(req, res) {
-    Stats
-      .find({}, {_id: 0, __v: 0})
-      .exec(function (err, stats) {
-        res.json({
-          stats: stats
-        });
+    var currentDate = new Date();
+    var lastMonth = new Date(currentDate.getTime());
+    lastMonth = lastMonth.setMonth(lastMonth.getMonth() - 1);
+    var lastQuarter = new Date(currentDate.getTime());
+    lastQuarter = lastQuarter.setMonth(lastQuarter.getMonth() -3);
+    Stats.aggregate([
+      {$match: {name: req.params.gameName}},
+      {$group: {
+        _id: {
+          $cond: { if: { $gt: [ "$collectionRun.date", new Date(lastMonth) ] }, then: {
+            year: {$year: "$collectionRun.date"},
+            month: {$month: "$collectionRun.date"},
+            day: {$dayOfYear: "$collectionRun.date"},
+            hour: {$hour: "$collectionRun.date"}
+          }, else: {
+            $cond: { if: { $gt: [ "$collectionRun.date", new Date(lastQuarter) ] }, then: {
+              year: {$year: "$collectionRun.date"},
+              month: {$month: "$collectionRun.date"},
+              day: {$dayOfYear: "$collectionRun.date"}
+            }, else: {
+                year: {$year: "$collectionRun.date"},
+                month: {$month: "$collectionRun.date"}
+            }}
+          }}
+        },
+        viewers: {$first: "$viewers"},
+        channels: {$first: "$channels"},
+        date: {$first: "$collectionRun.date"}
+      }},
+      {$project: {
+        _id: 0,
+        year: "$_id.year",
+        month: "$_id.month",
+        day: "$_id.day",
+        hour: "$_id.hour",
+        viewers: "$viewers",
+        channels: "$channels",
+        date: "$date",
+      }},
+      {$sort: {
+        year: 1,
+        month: 1,
+        day: 1,
+        hour: 1
+      }}
+    ]).exec(function (err, stats) {
+      res.json({
+        stats: stats
       });
+    });
   });
 
 module.exports = router;
